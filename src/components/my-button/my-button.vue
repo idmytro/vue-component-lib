c02e2d5fe47aeaffdf28b5fa8ad487c05660a8f5
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-07-02 00:01:02 +0300

    my-button

---
+++ src/components/my-button/my-button.vue
@@ -0,0 +1,11 @@
+<template>
+    <button type="button">my button</button>
+</template>
+
+<script>
+export default {
+    created () {
+        console.log('created my button');
+    }
+}
+</script>
