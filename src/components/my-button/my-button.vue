81fb67b5af3d3308a83af8b8296d7656f05b9d28
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-07-02 01:35:00 +0300

    try to finish

--- src/components/my-button/my-button.vue
+++ src/components/my-button/my-button.vue
@@ -1,11 +1,16 @@
 <template>
-    <button type="button">my button</button>
+    <button type="button" class="my-button">my button</button>
 </template>
 
 <script>
 export default {
+    name: 'my-button',
     created () {
         console.log('created my button');
     }
 }
 </script>
+
+<style>
+.my-button {color: red}
+</style>
